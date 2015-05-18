93ed326028746f9b78293ec68a5a29a6621accc4
MAriSaputra <snipstrikerbullraizer@gmail.com>
2015-05-18 22:55:17 +0800

    new edit class TestKebersihan

--- src/TestKebersihan.java
+++ src/TestKebersihan.java
@@ -2,21 +2,11 @@
 public class TestKebersihan {
 	public static void main(String[] args) {
 		
-	dataSirkulasiUdara sirkulasi = new dataSirkulasiUdara();
-	sirkulasi.data();
-	sirkulasi.AnalysisSirkulasiUdara();
-	
-	dataNilaiPencahayaan pencahayaan = new dataNilaiPencahayaan();
-	pencahayaan.data();
-	pencahayaan.AnalysisNilaiPencahayaan();
-	
-	dataKelembapan lembap = new dataKelembapan();
-	lembap.data();
-	lembap.AnalysisKelembapan();
-	
-	dataSuhu suhu = new dataSuhu();
-	suhu.data();
-	suhu.AnalysisSuhu();
+			AnalysisKebersihan kebersihan = new AnalysisKebersihan();
+				
+			kebersihan.data();
+			
+		}
 	
 }
-}
+
